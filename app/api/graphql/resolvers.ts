d0ab950c0d40d31631c2428ff2c5384a5c47e5de
rajvardhan <rajvardhanranawat80@gmail.com>
2024-09-25 19:58:41 +0530

    trial but didnt understand

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,3 +1,40 @@
-const resolvers = {}
+const resolvers = {
+  SearchType: {
+    __resolveType: (obj) => {
+    //   console.log(obj)
+      if (obj.species) {
+        return 'Animal'
+      }
+
+      return 'Person'
+    },
+  },
+  Person: {
+    name: (person) => {
+        console.log(person)
+      return 22
+    },
+  },
+  Query: {
+    search: () => {
+      return [
+        {
+          name: 'this is a name',
+          id: 'huw',
+        },
+        {
+          name: 'any',
+          species: 'Big cat',
+        },
+      ]
+    },
+    me: () => {
+      return 'me'
+    },
+    people: () => {
+      return [{ name: 'hitesh', id: 'njkwsd' }]
+    },
+  },
+}
 
 export default resolvers
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,5 +1,22 @@
 const schema = `#graphql
+type Person {
+    name : String!,
+    id : ID
+}
 
+type Animal {
+    species : String!
+    name : String!
+} 
+
+union SearchType = Person | Animal
+
+
+   type Query{
+        me:String!
+        people : [Person!]!
+        search :SearchType! 
+    }
 `
 
 export default schema
